386509d844f3189d29d02cc44eba7dcce0479dca
Lucas Eifler <lucass.eifler@gmail.comm>
2025-01-15 20:47:33 +0100

    creating MenuProfile page and components.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,25 @@
-import "./App.css";
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import MenuProfile from "./pages/MenuProfile";
 
-function App() {
+const App = () => {
+  const userData = {
+    avatar: "https://via.placeholder.com/50",
+    name: "Adriana",
+    institution: "Health Care Institute",
+    department: "Cardiology",
+  };
+
   return (
-    <>
-      <h1>Test</h1>
-    </>
+    <Router>
+      <Routes>
+        <Route
+          path="/menu-profile"
+          element={<MenuProfile userType="Chief" userData={userData} />}
+        />
+      </Routes>
+    </Router>
   );
-}
+};
 
 export default App;
